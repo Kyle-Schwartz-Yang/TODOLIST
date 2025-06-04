4b2c9c8d7162bb3b9fb04099a69f402c00f837db
schwartz <gtfsolo@gmail.com>
2025-06-04 21:35:41 +0300

    create state and onSubmitTodo

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,14 +1,41 @@
+import { FormEvent, useState } from "react";
+
 // ----------------------------------------------------------------
 import Header from "@components/widgets/Header/Header";
 import Footer from "@components/widgets/Footer/Footer";
-import ButtonShine from "../shared/kit/atoms/buttonShine/ButtonShine";
 
 // ----------------------------------------------------------------
 import "./App.scss";
 
 // =================================================================
 
+interface TodoItem {
+  id: number;
+  text: string;
+  complate: boolean;
+}
+
 export default function App() {
+  const [input, setInput] = useState<string>("");
+  const [todo, setTodo] = useState<TodoItem[]>([]);
+
+  const onChangeValue = (value: string) => {
+    setInput(value);
+  };
+
+  const onSubmitTodo = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const elementTodo: TodoItem = {
+      text: input,
+      id: Date.now(),
+      complate: false,
+    };
+
+    setTodo([...todo, elementTodo]);
+    setInput("");
+  };
+
   return (
     <>
       <Header></Header>
@@ -17,13 +44,20 @@ export default function App() {
           <div className="todo__container">
             <h1 className="todo__title">T❤DOLIST</h1>
 
-            <form action="#" className="todo__form">
-              <input type="text" placeholder="Create task" />
+            <form action="#" className="todo__form" onSubmit={onSubmitTodo}>
+              <input
+                type="text"
+                placeholder="Create task"
+                value={input}
+                onChange={(e) => onChangeValue(e.target.value)}
+              />
               <button type="submit">ADD</button>
             </form>
 
             <ul className="todo__list">
-              <TodoItem></TodoItem>
+              {todo.map((item) => (
+                <TodoItem key={item.id} task={item.text}></TodoItem>
+              ))}
             </ul>
           </div>
         </section>
@@ -36,7 +70,7 @@ export default function App() {
 function TodoItem(props: any) {
   return (
     <li className="todo__item">
-      <div className="todo__task-title">Create TodoList</div>
+      <div className="todo__task-title">{props.task}</div>
       <div className="todo__buttons">
         <button type="button" className="todo__button todo__button--done">
           DONE
